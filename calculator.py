dc5ce02c4a38f235df2f4ac738e25e414896ee01
propriyanka <priyankaw483@gmail.com>
2025-04-23 22:21:03 +0530

    Fix divide by zero bug

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import math
+
 class Calculator:
     def add(self, a, b):
         return a + b
@@ -10,13 +11,15 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
         return a / b
 
 if __name__ == "__main__":
     calculator = Calculator()
     num1 = 16
     num2 = 4
-    
+
     print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
